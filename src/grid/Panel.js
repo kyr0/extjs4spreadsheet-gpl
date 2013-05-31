ad1cb7ccd5b7c09b946c6cac4d350a2ef02d7c34
kyr0 <info@aron-homberg.de>
2013-05-31 16:28:32 +0200

    rem

--- src/grid/Panel.js
+++ src/grid/Panel.js
@@ -921,13 +921,13 @@ Ext.define('Spread.grid.Panel', {
             throw "You want the grid to be editable, but editing plugin isn't activated!";
         }
     },
-
     /**
      * @override
      * @protected
      * Re-Initializes editable setting of columns after reconfigure() has been called.
      * @return void
      */
+    /*
     reconfigure: function() {
 
         var ret = this.callOverridden(arguments);
@@ -937,7 +937,7 @@ Ext.define('Spread.grid.Panel', {
         }
         return ret;
     },
-
+    */
     /**
      * Enables/Disables the edit mode styling on the whole grid
      * @param {Boolean} editModeStyling Allow edit mode styling?
